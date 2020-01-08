ac9d011243913029f1973a20657883a1f5eab3d4
Nworah William <40431284+YongLiam@users.noreply.github.com>
2020-01-08 09:14:52 +0530

    Add files via upload
    android studio

--- MainActivity.java
+++ MainActivity.java
@@ -1,42 +1,133 @@
 package com.acecods.cscdemo;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.DownloadManager;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
 import android.print.PrintManager;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.DownloadListener;
 import android.webkit.GeolocationPermissions;
+import android.webkit.PermissionRequest;
 import android.webkit.URLUtil;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
     private Button btnSave;
+    private static String file_type     = "image/*";
     private static final int MY_PERMISSION_REQUEST_CODE = 123;
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private String cam_file_data = null;        // for storing camera file information
+    private ValueCallback<Uri> file_data;       // data/header received after file selection
+    private ValueCallback<Uri[]> file_path;     // received file(s) temp. location
+
+    private final static int file_req_code = 1;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(Build.VERSION.SDK_INT >= 21){
+            Uri[] results = null;
+
+            /*-- if file request cancelled; exited camera. we need to send null value to make future attempts workable --*/
+            if (resultCode == Activity.RESULT_CANCELED) {
+                if (requestCode == file_req_code) {
+                    file_path.onReceiveValue(null);
+                    return;
+                }
+            }
+
+            /*-- continue if response is positive --*/
+            if(resultCode== Activity.RESULT_OK){
+                if(requestCode == file_req_code){
+                    if(null == file_path){
+                        return;
+                    }
+
+                    ClipData clipData;
+                    String stringData;
+                    try {
+                        clipData = intent.getClipData();
+                        stringData = intent.getDataString();
+                    }catch (Exception e){
+                        clipData = null;
+                        stringData = null;
+                    }
+
+                    if (clipData == null && stringData == null && cam_file_data != null) {
+                        results = new Uri[]{Uri.parse(cam_file_data)};
+                    }else{
+                        if (clipData != null) { // checking if multiple files selected or not
+                            final int numSelectedFiles = clipData.getItemCount();
+                            results = new Uri[numSelectedFiles];
+                            for (int i = 0; i < clipData.getItemCount(); i++) {
+                                results[i] = clipData.getItemAt(i).getUri();
+                            }
+                        } else {
+                            results = new Uri[]{Uri.parse(stringData)};
+                        }
+                    }
+                }
+            }
+            file_path.onReceiveValue(results);
+            file_path = null;
+        }else{
+            if(requestCode == file_req_code){
+                if(null == file_data) return;
+                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                file_data.onReceiveValue(result);
+                file_data = null;
+            }
+        }
+    }
+
+    @SuppressLint({"SetJavaScriptEnabled", "WrongViewCast"})
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +140,7 @@ public class MainActivity extends AppCompatActivity {
         this.btnSave = (Button) findViewById(R.id.btnSave);
 
         webView.loadUrl("https://acecal.000webhostapp.com/goa/");
+        assert webView != null;
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
@@ -90,6 +182,9 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+
+
+
         ////////Geolocation Services - For getting Latitude and Longitude//////////
         ActivityCompat.requestPermissions(this, new String[]{
                 Manifest.permission.ACCESS_FINE_LOCATION,
@@ -97,11 +192,108 @@ public class MainActivity extends AppCompatActivity {
         }, 0);
 
         webView.setWebChromeClient(new WebChromeClient() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
             @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        request.grant(request.getResources());
+                    }
+                });
+            }
+
+
+            public void onPermissionRequestCanceled(PermissionRequest request) {
+                Log.d(TAG, "onPermissionRequestCanceled");
+            }
+
+            /*-- handling input[type="file"] requests for android API 21+ --*/
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+
+                if(file_permission() && Build.VERSION.SDK_INT >= 21) {
+                    file_path = filePathCallback;
+                    Intent takePictureIntent = null;
+                    Intent takeVideoIntent = null;
+
+                    boolean includeVideo = false;
+                    boolean includePhoto = false;
+
+                    /*-- checking the accept parameter to determine which intent(s) to include --*/
+                    paramCheck:
+                    for (String acceptTypes : fileChooserParams.getAcceptTypes()) {
+                        String[] splitTypes = acceptTypes.split(", ?+"); // although it's an array, it still seems to be the whole value; split it out into chunks so that we can detect multiple values
+                        for (String acceptType : splitTypes) {
+                            switch (acceptType) {
+                                case "*/*":
+                                    includePhoto = true;
+                                    break paramCheck;
+                                case "image/*":
+                                    includePhoto = true;
+                                    break;
+                            }
+                        }
+                    }
+
+                    if (fileChooserParams.getAcceptTypes().length == 0) {   //no `accept` parameter was specified, allow both photo and video
+                        includePhoto = true;
+                       // includeVideo = true;
+                    }
+
+                    if (includePhoto) {
+                        takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if (takePictureIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
+                            File photoFile = null;
+                            try {
+                                photoFile = create_image();
+
+                                takePictureIntent.putExtra("PhotoPath", cam_file_data);
+                            } catch (IOException ex) {
+                                Log.e(TAG, "Image file creation failed", ex);
+                            }
+                            if (photoFile != null) {
+                                cam_file_data = "file:" + photoFile.getAbsolutePath();
+                                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                            } else {
+                                cam_file_data = null;
+                                takePictureIntent = null;
+                            }
+                        }
+                    }
+
+                    Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                    contentSelectionIntent.setType(file_type);
+
+
+                    Intent[] intentArray;
+                    if (takePictureIntent != null && takeVideoIntent != null) {
+                        intentArray = new Intent[]{takePictureIntent, takeVideoIntent};
+                    } else if (takePictureIntent != null) {
+                        intentArray = new Intent[]{takePictureIntent};
+                    } else if (takeVideoIntent != null) {
+                        intentArray = new Intent[]{takeVideoIntent};
+                    } else {
+                        intentArray = new Intent[0];
+                    }
+
+                    Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                    chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                    chooserIntent.putExtra(Intent.EXTRA_TITLE, "File chooser");
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                    startActivityForResult(chooserIntent, file_req_code);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
             public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
                 callback.invoke(origin, true, false);
             }
         });
+
+
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -247,6 +439,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /*-- callback reporting if error occurs --*/
+    public class Callback extends WebViewClient{
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+            Toast.makeText(getApplicationContext(), "Failed loading app!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*-- checking and asking for required file permissions --*/
+    public boolean file_permission(){
+        if(Build.VERSION.SDK_INT >=23 && (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    /*-- creating new image file here --*/
+    private File create_image() throws IOException{
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img_"+timeStamp+"_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName,".jpg",storageDir);
+    }
+
+    /*-- creating new video file here --*/
+    private File create_video() throws IOException {
+        @SuppressLint("SimpleDateFormat")
+        String file_name    = new SimpleDateFormat("yyyy_mm_ss").format(new Date());
+        String new_name     = "file_"+file_name+"_";
+        File sd_directory   = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(new_name, ".3gp", sd_directory);
+    }
+
 
     protected void checkPermission(){
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
@@ -334,6 +560,11 @@ public class MainActivity extends AppCompatActivity {
         } else {
 
         }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
     }
 
 }
